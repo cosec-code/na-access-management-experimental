d39bdd0ccc3f99accda61550f5fcdb9fd0d43d03
Pranav Kishan <51042522+PranavKishan213@users.noreply.github.com>
2020-05-23 14:54:08 +0530

    Add files via upload
    Pure reader for the RFID.

---
+++ read_integrated.py
@@ -0,0 +1,62 @@
+import RPi.GPIO as GPIO
+import MFRC522
+import signal
+import time
+import csv
+import pandas
+
+continue_reading = True
+
+
+# Capture SIGINT for cleanup when the script is aborted
+def end_read(signal, frame):
+    global continue_reading
+    print("Ctrl+C captured, ending read.")
+    continue_reading = False
+    GPIO.cleanup()
+
+
+# Hook the SIGINT
+signal.signal(signal.SIGINT, end_read)
+
+# Create an object of the class MFRC522
+MIFAREReader = MFRC522.MFRC522()
+
+# Welcome message
+print("Welcome to the MFRC522 data read example")
+print("Press Ctrl-C to stop.")
+
+# This loop keeps checking for chips. If one is near it will get the UID and authenticate
+while continue_reading:
+
+    # Scan for cards
+    (status, TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+
+    # If a card is found
+    if status == MIFAREReader.MI_OK:
+        print("Card detected")
+
+    # Get the UID of the card
+    (status, uid) = MIFAREReader.MFRC522_Anticoll()
+
+    # If we have the UID, continue
+    if status == MIFAREReader.MI_OK:
+
+        # Print UID
+        my_uid_string = "{},{},{},{},{}".format(str(uid[0]), str(uid[1]), str(uid[2]), str(uid[3]), str(uid[4]))
+
+        # Configure LED Output Pin
+        LED = 18
+        GPIO.setup(LED, GPIO.OUT)
+        GPIO.output(LED, GPIO.LOW)
+
+        # Check to see if card UID read matches your card UID
+
+        if uid == my_uid_string:
+            print("Access Granted")
+            GPIO.output(LED, GPIO.HIGH)  # Turn on LED
+            time.sleep(5)  # Wait 5 Seconds
+            GPIO.output(LED, GPIO.LOW)  # Turn off LED
+
+        else:  # Don't open if UIDs don't match
+            print("Access Denied, YOU SHALL NOT PASS!")
