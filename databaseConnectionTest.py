6739a14f51405ed7d82d843b708df4cd7287f8fd
crof2003 <20980923+crof2003@users.noreply.github.com>
2020-05-24 18:12:27 -0400

    Updated the SQLTest file
    Updated the MySQL connector test file

--- databaseConnectionTest.py
+++ databaseConnectionTest.py
@@ -1,17 +1,28 @@
 import mysql.connector
 
+# TODO: do not store username/password in code files.  This user has read-only access to a database of fake information.
 cnx = mysql.connector.connect(user='crof2003_AccessManagementReadDev', password='hoL9dBuIKVq8uKL479DTEpCPET99', host='johnny.heliohost.org', database='crof2003_NA_Access_Management_Dev01')
 cursor = cnx.cursor()
 
-query = ("SELECT access_Code, Authorisation_Level, Name, RFID FROM AccessKeysTest WHERE RFID = \"")
+#loop through taking multiple codes
+inRFID = "-1"
+csvLoaded = False
 
-inRFID = "51,37,7,973,53"
+while not inRFID == "":
 
-#cursor.execute(query, inRFID)
-cursor.execute(query + inRFID + "\"")
+    #inRFID = "51,37,7,973,53"
+    inRFID = input(str("Enter your code in the format (XX,XX,X,XXX,XX) or blank to quit: "))
 
-for (access_Code, Authorisation_Level, Name, RFID) in cursor:
-  print("{} has auth level: {}.  RFID:{}".format( Name, Authorisation_Level, RFID))
+    # Check if the program should be ended by blank user input
+    if inRFID == "":
+        break
+
+    #cursor.execute(query, inRFID)
+    query = ("SELECT access_Code, Authorisation_Level, Name, RFID FROM AccessKeysTest WHERE RFID = \"")
+    cursor.execute(query + inRFID + "\"")
+
+    for (access_Code, Authorisation_Level, Name, RFID) in cursor:
+        print("{} has auth level: {}.  RFID:{}".format( Name, Authorisation_Level, RFID))
 
 cursor.close()
 cnx.close()
